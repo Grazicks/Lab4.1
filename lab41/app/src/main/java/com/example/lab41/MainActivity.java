b31104d202ecb910aad7edb2dcf6f2a032843028
Grazicks <rodion.tarasov.03@mail.ru>
2023-12-22 14:45:07 +0700

    Lab4.1
    Добавления действия при нажатии кнопки далее

--- lab41/app/src/main/java/com/example/lab41/MainActivity.java
+++ lab41/app/src/main/java/com/example/lab41/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab41;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -15,20 +17,19 @@ public class MainActivity extends AppCompatActivity {
     int quest_limit = 5;
     int tr_ans = 0;
     private static int array_ind, quest_ind;
-    Button trueButton, falseButton, nextButton;
+    Button trueButton, falseButton, nextButton, beforeButton;
     String quest_value = getQuestion_value(array_ind, quest_ind);
     String question_key = "QUESTION_VALUE_SERIALIZABLE_KEY";
-    private int question_value;
 
     public static String getQuestion_value(int arr_index, int q_index) {
-        String question_value;
+        String quest_value;
         Random rand = new Random();
         int min = 0; // Min value
         int max = 1; // Max value
         array_ind = rand.nextInt(20);
         quest_ind = 0;
-        question_value = QuestionDataBase.GetQuestion(array_ind, quest_ind);
-        return question_value;
+        quest_value = QuestionDataBase.GetQuestion(array_ind, quest_ind);
+        return quest_value;
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +40,13 @@ public class MainActivity extends AppCompatActivity {
         trueButton = findViewById(R.id.TrueButton);
         falseButton = findViewById(R.id.FalseButton);
         nextButton = findViewById(R.id.Next);
+        beforeButton = findViewById(R.id.Before);
 
         View nextButton = findViewById(R.id.Next);
         nextButton.setEnabled(false);
-        question.setText(question_value);
+        View beforeButton = findViewById(R.id.Before);
+        beforeButton.setEnabled(false);
+        question.setText(quest_value);
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,5 +75,45 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View trueButton = findViewById(R.id.TrueButton);
+                trueButton.setEnabled(true);
+                View falseButton = findViewById(R.id.FalseButton);
+                falseButton.setEnabled(true);
+                View beforeButton = findViewById(R.id.Before);
+                beforeButton.setEnabled(true);
+                quest_count++;
+                if (quest_count == quest_limit) {
+                    String answers_count = "Правильных ответов: " + tr_ans;
+                    Toast.makeText(MainActivity.this, answers_count, Toast.LENGTH_LONG).show();
+                    view.setEnabled(false);
+                    trueButton.setEnabled(false);
+                    falseButton.setEnabled(false);
+                }
+                else {
+                    Random rand = new Random();
+                    array_ind = rand.nextInt(20);
+                    quest_ind += 1;
+                    quest_value = QuestionDataBase.GetQuestion(array_ind, quest_ind);
+                    question.setText(quest_value);
+                    view.setEnabled(false);
+                }
+            }
+        });
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("question_value", quest_value);
+    }
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        quest_value = savedInstanceState.getString("question_value");
+        TextView question = findViewById(R.id.Question);
+        question.setGravity(Gravity.CENTER);
+        question.setText(quest_value);
     }
 }
