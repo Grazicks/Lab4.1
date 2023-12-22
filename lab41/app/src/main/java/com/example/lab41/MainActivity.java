f6581e2d32ebf11ef87830788483ec312e8b1d73
Grazicks <153579018+Grazicks@users.noreply.github.com>
2023-12-22 13:59:04 +0700

    Add files via upload
    Добавление действия на кнопки true и false

--- lab41/app/src/main/java/com/example/lab41/MainActivity.java
+++ lab41/app/src/main/java/com/example/lab41/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.lab41;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button trueButton, falseButton, nextButton;
     String quest_value = getQuestion_value(array_ind, quest_ind);
     String question_key = "QUESTION_VALUE_SERIALIZABLE_KEY";
+    private int question_value;
 
     public static String getQuestion_value(int arr_index, int q_index) {
         String question_value;
@@ -30,5 +34,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView question = findViewById(R.id.Question);
+        question.setGravity(Gravity.CENTER);
+        trueButton = findViewById(R.id.TrueButton);
+        falseButton = findViewById(R.id.FalseButton);
+        nextButton = findViewById(R.id.Next);
+
+        View nextButton = findViewById(R.id.Next);
+        nextButton.setEnabled(false);
+        question.setText(question_value);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View trueButton = findViewById(R.id.TrueButton);
+                trueButton.setEnabled(false);
+                View falseButton = findViewById(R.id.FalseButton);
+                falseButton.setEnabled(false);
+                View nextButton = findViewById(R.id.Next);
+                nextButton.setEnabled(true);
+                if (array_ind < 10) {
+                    tr_ans++;
+                }
+            }
+        });
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View trueButton = findViewById(R.id.TrueButton);
+                trueButton.setEnabled(false);
+                View falseButton = findViewById(R.id.FalseButton);
+                falseButton.setEnabled(false);
+                View nextButton = findViewById(R.id.Next);
+                nextButton.setEnabled(true);
+                if (array_ind >= 10) {
+                    tr_ans++;
+                }
+            }
+        });
     }
 }
